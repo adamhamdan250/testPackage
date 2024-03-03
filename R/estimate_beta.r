83b8bff816c7766c52b3fadbab8543e96d2abf37
adamhamdan250 <adam_hamdan@brown.edu>
2024-03-03 10:41:34 -0500

    new functions

---
+++ R/estimate_beta.r
@@ -0,0 +1,14 @@
+#' My first function
+#'
+#' @param y an n x 1 vector of observations
+#' @param X an n x k vector of observations
+#'
+#' @return beta_hat An estimate of beta in y = beta X + epsilon
+#' @export
+#'
+#' @examples
+#' myfunc(3, 5)
+myfunc <- function(y, X) {
+  result <- (t(X) %*% X)^(-1) %*%(X %*% Y)
+  return(result)
+}
